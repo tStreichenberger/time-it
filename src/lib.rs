b93ff13fd08955c0ba27ea618099e4e2806c31c6
tStreichenberger <tristan@thehive.ai>
2023-05-12 09:24:58 -0700

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,6 @@ extern crate syn;
 use proc_macro::TokenStream;
 
 #[proc_macro]
-pub fn make_answer(_item: TokenStream) -> TokenStream {
-    "fn answer() -> u32 { 42 }".parse().unwrap()
-}
-
-#[proc_macro]
 pub fn time_it(to_time: TokenStream) -> TokenStream {
     let to_time = syn::Item::Verbatim(to_time.into());
 
