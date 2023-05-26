47181216ee71ef20803b320894c0d40b12aca5fd
tStreichenberger <tristan@thehive.ai>
2023-05-26 15:55:40 -0700

    added unique names to start labels

--- time-it-macros/src/lib.rs
+++ time-it-macros/src/lib.rs
@@ -9,10 +9,14 @@ use proc_macro::TokenStream;
 pub fn time_it(to_time: TokenStream) -> TokenStream {
     let to_time = syn::Item::Verbatim(to_time.into());
 
+    let random_name = uuid::Uuid::new_v4().to_string().replace("-", "");
+
+    let start_ident = syn::Ident::new(&format!("start_{}", random_name), proc_macro::Span::call_site().into());
+
     quote! {
-        let start = std::time::Instant::now();
+        let #start_ident = std::time::Instant::now();
         #to_time
-        println!("Operation took {}ms", start.elapsed().as_millis())
+        println!("Operation took {}ms", #start_ident.elapsed().as_millis())
     }
     .into()
 }
