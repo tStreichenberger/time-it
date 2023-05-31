046e8cd180cef253c8a99412b5e5c356a4f956dc
tStreichenberger <tristan@thehive.ai>
2023-05-30 21:01:13 -0700

    feat: changed time_fn impl to use drop timer

--- time-it-macros/src/lib.rs
+++ time-it-macros/src/lib.rs
@@ -41,86 +41,22 @@ pub fn time_it(to_time: TokenStream) -> TokenStream {
     .into()
 }
 
-// TODO: fix the unused code warning to not reveal inner func
 #[proc_macro_attribute]
 pub fn time_fn(_: TokenStream, item: TokenStream) -> TokenStream {
-    let mut old_fn = parse_macro_input!(item as syn::ItemFn);
-    let mut new_fn = old_fn.clone();
+    let mut func = syn::parse_macro_input!(item as syn::ItemFn);
+    let name = (&func.sig.ident).to_string();
+    let name = name.as_str();
 
-    // Rename existing function to something that should not have any collisions
-    let old_name = old_fn.sig.ident.clone();
-    let mut old_name_string = old_name.to_string();
-    old_name_string.push_str("_time_it_fn_inner");
-    let hidden_name = Ident::new(
-        old_name_string.as_str(),
-        proc_macro::Span::call_site().into(),
-    );
-    old_fn.sig.ident = hidden_name.clone();
+    let timer_name = get_random_name();
 
-    // parse out the inputs of the function so we can call them in the new function
-    let inputs_with_types = old_fn.sig.inputs.clone();
-    let just_args: syn::punctuated::Punctuated<syn::Pat, syn::token::Comma> = inputs_with_types
-        .iter()
-        .map(|arg| match arg {
-            syn::FnArg::Typed(pat_typed) => *pat_typed.pat.clone(),
-            syn::FnArg::Receiver(..) => panic!("#[time_fn] does not support methods"), //TODO: add support for this
-        })
-        .collect();
+    let start_timer_tokens = quote!{ let #timer_name = time_it::Timer(Some(#name), ::std::time::Instant::now()); }.into();
+    let start_time_stmt = syn::parse_macro_input!(start_timer_tokens as syn::Stmt);
 
-    // look for and remove lifetime from generics since we won't use it to call inner func
-    // let mut found_lifetime = None;
-    let mut generics = old_fn.sig.generics.clone();
-    generics.params = generics
-        .params
-        .into_iter()
-        .filter(|gen| match gen {
-            syn::GenericParam::Lifetime(_) => {
-                // found_lifetime = Some(lifetime.clone());
-                false
-            }
-            _ => true,
-        })
-        .collect();
+    func.block.stmts.insert(0, start_time_stmt);
 
-    // fetch generic arguments to use them to call hidden function
-    let (_, type_generics, _) = generics.split_for_impl();
-    let turbofish = type_generics.as_turbofish();
-
-    // get await if it is an async function
-    let maybe_await = match old_fn.sig.asyncness {
-        Some(..) => quote! {.await},
-        None => quote! {},
-    };
-
-    let start_ident = get_random_name();
-
-    let tag = old_name.to_string(); //TODO: allow for optional msg arg
-    let tag = tag.as_str();
-
-    // create body of new function which calls old function and converts error
-    let new_fn_code_tokens: TokenStream = quote! {
-        {
-            let #start_ident = std::time::Instant::now();
-            let output = #hidden_name #turbofish (#just_args) #maybe_await;
-            time_it::action(Some(#tag), #start_ident.elapsed());
-            output
-        }
-    }
-    .into();
-
-    let new_fn_code = parse_macro_input!(new_fn_code_tokens as syn::Block);
-
-    new_fn.block = Box::new(new_fn_code);
-
-    // write out the two functions
-    let new_code: TokenStream = quote! {
-        #new_fn
-        #[inline(always)]
-        #old_fn
-    }
-    .into();
-
-    new_code
+    quote! {
+        #func
+    }.into()
 }
 
 /// generates a random identifier to be used as the start instant name to avoid overwritting any other vars
@@ -128,7 +64,7 @@ fn get_random_name() -> Ident {
     let random_name = uuid::Uuid::new_v4().to_string().replace("-", "");
 
     Ident::new(
-        &format!("start_{}", random_name),
+        &format!("_start_{}", random_name),
         proc_macro::Span::call_site().into(),
     )
 }
--- time-it/src/lib.rs
+++ time-it/src/lib.rs
@@ -3,3 +3,14 @@ extern crate time_it_macros;
 
 pub use time_it_builder::*;
 pub use time_it_macros::*;
+
+
+/// TODO: probably just make this an Option String if we want to make this part of public api
+pub struct Timer(pub Option<&'static str>, pub std::time::Instant);
+
+impl Drop for Timer {
+    fn drop(&mut self) {
+        // TODO: replace with run
+        action(self.0, self.1.elapsed());
+    }
+}
