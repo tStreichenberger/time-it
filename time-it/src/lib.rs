1077e4280549d427f1ec8fa94661820f4e17c405
tStreichenberger <tristan@thehive.ai>
2023-06-03 19:26:09 -0700

    action -> run

--- time-it/src/lib.rs
+++ time-it/src/lib.rs
@@ -10,6 +10,6 @@ pub struct Timer(pub Option<&'static str>, pub std::time::Instant);
 impl Drop for Timer {
     fn drop(&mut self) {
         // TODO: replace with run
-        action(self.0, self.1.elapsed());
+        run(self.0, self.1.elapsed());
     }
 }
